1341674d1b29e671feae41ccaf205f5e1498e339
Momoh Nobert <okhai0112@gmail.com>
2022-12-24 21:56:05 +0100

    created navigation component

---
+++ src/app/components/NavigationPreview/navigationPreview.styles.tsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const Style = {
+  Container: styled.div`
+    display: flex;
+    height: 100%;
+    width: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    button {
+      padding: 10px;
+      border-radius: 50%;
+      border: 0.5px solid rgba(0,0,0,0.4);
+      display: grid;
+      place-items: center;
+      background-color: white;
+      transition: border 0.2s linear;
+
+      svg {
+        height: 15px;
+        width: 15px;
+        transition: scale 0.2s linear;
+      }
+
+      &:hover {
+        border: 0.5px solid black;
+        svg {
+          scale: 1.1;
+        }
+      }
+    }
+  `
+}
